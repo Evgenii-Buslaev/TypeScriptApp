71d7b351eb067ecbe56dc3d9baa4276d8b09d9c1
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-11-11 16:46:23 +0500

    Created hook useAction

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useAction } from "../hooks/useAction";
 import { useTypedSelector } from "../hooks/usedTypedSelector";
 import { fetchUsers } from "../store/action-creators/user";
 
 const UserList: React.FC = () => {
   const { users, error, loading } = useTypedSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const { fetchUsers } = useAction();
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    fetchUsers();
   }, []);
 
   if (loading) {
